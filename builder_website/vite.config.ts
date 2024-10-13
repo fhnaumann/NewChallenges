5f681b44e09dc2be119f1098cf732b7e00b0157e
Felix Naumann <wand516@gmail.com>
2024-10-13 21:00:15 +0200

    install criteria interfaces in CI 2

--- builder_website/vite.config.ts
+++ builder_website/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +17,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@public': fileURLToPath(new URL('./public', import.meta.url))
+      '@public': fileURLToPath(new URL('./public', import.meta.url)),
+      'criteria-interfaces': path.resolve(__dirname, '../criteria-interfaces')
     }
   }
 })
