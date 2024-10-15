4f0144c147ee9b4d35b1b8bf862191fb13682de7
Felix Naumann <wand516@gmail.com>
2024-10-15 15:31:25 +0200

    add aws credentials for plugin integration tests

--- plugin/src/test/java/wand555/github/io/challenges/integration/IntegrationTestSetupExtension.java
+++ plugin/src/test/java/wand555/github/io/challenges/integration/IntegrationTestSetupExtension.java
@@ -12,7 +12,7 @@ public class IntegrationTestSetupExtension implements BeforeAllCallback {
             Assertions.fail("Stage variable not set to 'testing' when running the test suite. Aborting!");
         }
         DynamoDBHelper.clearDynamoDBTestingTable();
-        // A comment
+        // A comment 2
         // TODO: clear s3
 
         DynamoDBHelper.setUpEmptyDynamoDBTestingTable();
