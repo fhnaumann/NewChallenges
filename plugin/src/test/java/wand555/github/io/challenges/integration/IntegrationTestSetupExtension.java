f3f92fbeecda29536df8d5c436b25132a2a5f4ff
Felix Naumann <wand516@gmail.com>
2024-10-15 17:40:08 +0200

    debug node auth token

--- plugin/src/test/java/wand555/github/io/challenges/integration/IntegrationTestSetupExtension.java
+++ plugin/src/test/java/wand555/github/io/challenges/integration/IntegrationTestSetupExtension.java
@@ -12,7 +12,7 @@ public class IntegrationTestSetupExtension implements BeforeAllCallback {
             Assertions.fail("Stage variable not set to 'testing' when running the test suite. Aborting!");
         }
         DynamoDBHelper.clearDynamoDBTestingTable();
-        // A comment 4
+        // A comment 5
         // TODO: clear s3
 
         DynamoDBHelper.setUpEmptyDynamoDBTestingTable();
